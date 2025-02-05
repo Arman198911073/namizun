4c95a02c757f3e9fb749becc620d942bfc94ba90
Arman198911073 <arman.mohammadi198911073@gmail.com>
2025-02-06 00:01:42 +0330

    Update setup.py

--- namizun_menu/setup.py
+++ namizun_menu/setup.py
@@ -1,14 +1,17 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(name='namizun_menu',
-      version='1.0.0',
-      description='namizun menu',
-      author='MalKeMit',
-      author_email='khodemalkemit@gmail.com',
-      url='https://github.com/malkemit/namizun',
-      setup_requires=['wheel'],
-      install_requires=['colored~=1.4.4',
-                        'pyfiglet~=0.8.post1',
-                        'prettytable~=3.5.0'],
-      py_modules=['time', 'network', 'udp', 'database', 'log', 'ip']
-      )
+setup(
+    name='namizun_core',
+    version='1.3.8',
+    description='namizun main functions',
+    author='MalKeMit',
+    author_email='khodemalkemit@gmail.com',
+    url='https://github.com/malkemit/namizun',
+    setup_requires=['wheel'],
+    install_requires=[
+        'psutil==5.9.4',
+        'redis==4.3.5',
+        'pytz==2022.6'
+    ],
+    py_modules=['time', 'network', 'udp', 'database', 'log', 'ip']
+)
