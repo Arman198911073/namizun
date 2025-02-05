1e92b994e6b0177f1c19667e97700ab88c6d0fbd
Arman198911073 <arman.mohammadi198911073@gmail.com>
2025-02-05 23:47:59 +0330

    Update setup.py

--- namizun_core/setup.py
+++ namizun_core/setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='namizun_core',
       version='1.3.8',
@@ -7,7 +7,7 @@ setup(name='namizun_core',
       author_email='khodemalkemit@gmail.com',
       url='https://github.com/malkemit/namizun',
       setup_requires=['wheel'],
-      py_modules=["database", "udp", "time", "log", "network", "ip"],
+      packages=find_packages(),
       install_requires=['psutil==5.9.4',
                         'redis==4.3.5',
                         'pytz==2022.6']
